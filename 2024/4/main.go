3e6d8551b16638e524f08b1e3ececd6f27ba3c97
apwic <adiyansawicaksana@gmail.com>
2024-12-04 17:10:59 +0700

    day 4 part 2

--- 2024/4/main.go
+++ 2024/4/main.go
@@ -27,6 +27,21 @@ func check(grid [][]string, i int, j int, dx int, dy int) bool {
 	return b.String() == target
 }
 
+func check2(grid [][]string, i int, j int) bool {
+	if !valid(len(grid), len(grid[0]), i+2, j+2) {
+		return false
+	}
+
+	var b1, b2 strings.Builder
+
+	for k := 0; k < 3; k++ {
+		b1.WriteString(grid[i+k][j+k])
+		b2.WriteString(grid[i+k][j+2-k])
+	}
+
+	return (b1.String() == "MAS" || b1.String() == "SAM") && (b2.String() == "MAS" || b2.String() == "SAM")
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -50,6 +65,7 @@ func main() {
 	}
 
 	puzzle_1 := 0
+	puzzle_2 := 0
 	directions := [8][2]int{
 		{-1, -1},
 		{-1, 0},
@@ -73,8 +89,13 @@ func main() {
 					puzzle_1++
 				}
 			}
+
+			if check2(grid, i, j) {
+				puzzle_2++
+			}
 		}
 	}
 
 	fmt.Println("puzzle 1: ", puzzle_1)
+	fmt.Println("puzzle 2: ", puzzle_2)
 }
