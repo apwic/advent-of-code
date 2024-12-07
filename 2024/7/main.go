d553e91458a8a57060094bec9c9e7275e210a245
apwic <adiyansawicaksana@gmail.com>
2024-12-07 17:45:05 +0700

    2024 day 7 part 2

--- 2024/7/main.go
+++ 2024/7/main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+func digits(num int) int {
+	if num == 0 {
+		return 1
+	}
+
+	count := 0
+	for num != 0 {
+		num /= 10
+		count++
+	}
+
+	return count
+}
 
 func backtrack(nums []int, idx int, curr int, target int) bool {
 	if idx == len(nums) {
@@ -19,7 +35,20 @@ func backtrack(nums []int, idx int, curr int, target int) bool {
 	return backtrack(nums, idx+1, add, target) || backtrack(nums, idx+1, mul, target)
 }
 
+func backtrack2(nums []int, idx int, curr int, target int) bool {
+	if idx == len(nums) {
+		return curr == target
+	}
+
+	add := curr + nums[idx]
+	mul := curr * nums[idx]
+	comb := curr*(int(math.Pow(10, float64(digits(nums[idx]))))) + nums[idx]
+
+	return backtrack2(nums, idx+1, add, target) || backtrack2(nums, idx+1, mul, target) || backtrack2(nums, idx+1, comb, target)
+}
+
 func main() {
+	start := time.Now()
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -61,6 +90,7 @@ func main() {
 	}
 
 	puzzle_1 := 0
+	puzzle_2 := 0
 	for idx := range grid {
 		nums := grid[idx]
 		target := targets[idx]
@@ -68,7 +98,15 @@ func main() {
 		if backtrack(nums, 1, nums[0], target) {
 			puzzle_1 += target
 		}
+
+		if backtrack2(nums, 1, nums[0], target) {
+			puzzle_2 += target
+		}
 	}
 
+	elapsed := time.Since(start)
+	fmt.Println("Time elapsed :", elapsed)
+
 	fmt.Println("puzzle 1", puzzle_1)
+	fmt.Println("puzzle 2", puzzle_2)
 }
