5f4c6cb84c88bd11ec9f8ea096219a4cbf7e84de
apwic <adiyansawicaksana@gmail.com>
2024-12-12 20:28:29 +0700

    2024 day 12 part 2

--- 2024/12/main.go
+++ 2024/12/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type Pos struct {
@@ -55,14 +56,19 @@ func valid(m, n, i, j int) bool {
 	return 0 <= i && i < m && 0 <= j && j < n
 }
 
-func createRegion(grid [][]string, visited *map[Pos]bool, i, j int) (int, int) {
+func createRegion(grid [][]string, visited *map[Pos]bool, i, j int) (int, int, int) {
 	m, n := len(grid), len(grid[0])
 	name := grid[i][j]
 	queue := []Pos{{i, j}}
 
 	(*visited)[Pos{x: i, y: j}] = true
 
-	area, perimeter := 0, 0
+	area, perimeter, corner := 0, 0, 0
+
+	// diagonal directions to check corners
+	diagonalDirs := []Pos{
+		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
+	}
 
 	for len(queue) > 0 {
 		pop := queue[0]
@@ -70,6 +76,29 @@ func createRegion(grid [][]string, visited *map[Pos]bool, i, j int) (int, int) {
 		x, y := pop.x, pop.y
 
 		area++
+
+		// check for corners
+		for _, d := range diagonalDirs {
+			diag := Pos{x: x + d.x, y: y + d.y}
+			adj1 := Pos{x: x + d.x, y: y}
+			adj2 := Pos{x: x, y: y + d.y}
+
+			validDiag := valid(m, n, diag.x, diag.y)
+			validAdj1 := valid(m, n, adj1.x, adj1.y)
+			validAdj2 := valid(m, n, adj2.x, adj2.y)
+
+			outOfBounds := !validDiag && !validAdj1 && !validAdj2
+
+			checkAdj1 := !validAdj1 || (validAdj1 && grid[adj1.x][adj1.y] != name)
+
+			checkAdj2 := !validAdj2 || (validAdj2 && grid[adj2.x][adj2.y] != name)
+
+			checkDiag := validDiag && grid[diag.x][diag.y] != name && !checkAdj1 && !checkAdj2
+
+			if outOfBounds || (checkAdj1 && checkAdj2) || checkDiag {
+				corner++
+			}
+		}
 
 		for _, d := range directions {
 			next := Pos{x: x + d.x, y: y + d.y}
@@ -87,12 +116,13 @@ func createRegion(grid [][]string, visited *map[Pos]bool, i, j int) (int, int) {
 				perimeter++
 			}
 		}
+
 	}
 
-	return area, perimeter
+	return area, perimeter, corner
 }
 
-func findRegion(grid [][]string) int {
+func findRegion(grid [][]string) (int, int) {
 	m, n := len(grid), len(grid[0])
 	visited := make(map[Pos]bool)
 	for i := 0; i < m; i++ {
@@ -101,26 +131,32 @@ func findRegion(grid [][]string) int {
 		}
 	}
 
-	price := 0
+	price1 := 0
+	price2 := 0
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if visited[Pos{x: i, y: j}] {
 				continue
 			}
-			area, param := createRegion(grid, &visited, i, j)
-			price += area * param
+			area, param, corner := createRegion(grid, &visited, i, j)
+			price1 += area * param
+			price2 += area * corner
 		}
 	}
 
-	return price
+	return price1, price2
 }
 
 func main() {
+	start := time.Now()
 	grid, err := parseInput("input.txt")
 	if err != nil {
 		return
 	}
 
-	puzzle_1 := findRegion(grid)
+	puzzle_1, puzzle_2 := findRegion(grid)
+
+	fmt.Println("time elapsed:", time.Since(start))
 	fmt.Println("puzzle 1:", puzzle_1)
+	fmt.Println("puzzle 2:", puzzle_2)
 }
