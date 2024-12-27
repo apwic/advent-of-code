e3f201182964d827a1b2192cbae7a41e30a5bb9e
apwic <adiyansawicaksana@gmail.com>
2024-12-27 16:46:20 +0700

    2024 day 23 part 2

--- 2024/23/README.md
+++ 2024/23/README.md
@@ -2,8 +2,16 @@
 
 ### Part 1
 
-Given input of pair that's separated by `-`, for exampel `kh-tc` and `qp-kh`.
-Each of the piar represents undirected graph that is connecting two nodes.
+Given input of pair that's separated by `-`, for example `kh-tc` and `qp-kh`.
+Each of the pair represents undirected graph that is connecting two nodes.
 We need to find a cyclic component with the size of 3. On top of that, we need
 to find in the cyclic component the node that starts with `t`. Quite simple,
 just iterating through the neighbor until get back to the starting node.
+
+### Part 2
+
+Part 2 is a lot different than the Part 1. In this part 2, we are asked to
+find the largest interconnected node. I looked around and apparently this
+largest interconnected is called _maximum clique_. The algorithm to find this
+maximum clique is called Bron-Kerbosch algorithm. Using that algorithm it is
+quite simple to get the answer.
--- 2024/23/main.go
+++ 2024/23/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"slices"
 	"strings"
+	"time"
 )
 
 const (
@@ -58,7 +59,67 @@ func findCyclic(startNode, currNode string, length int, path []string) {
 	}
 }
 
+// Bron-Kerbosch Algorithm
+func bronKerbosch(R, P, X map[string]bool, maxClique *map[string]bool) {
+	if len(P) == 0 && len(X) == 0 {
+		if len(R) > len(*maxClique) {
+			newClique := make(map[string]bool)
+			for key := range R {
+				(newClique)[key] = true
+			}
+			(*maxClique) = newClique
+		}
+		return
+	}
+
+	for key := range P {
+		newR := make(map[string]bool)
+		for node := range R {
+			newR[node] = true
+		}
+		newR[key] = true
+
+		newP := make(map[string]bool)
+		newX := make(map[string]bool)
+		for _, neighbor := range graphs[key] {
+			if P[neighbor] {
+				newP[neighbor] = true
+			}
+			if X[neighbor] {
+				newX[neighbor] = true
+			}
+		}
+
+		bronKerbosch(newR, newP, newX, maxClique)
+
+		delete(P, key)
+		X[key] = true
+	}
+}
+
+func findInterconnected() []string {
+	R := make(map[string]bool)
+	P := make(map[string]bool)
+	X := make(map[string]bool)
+
+	for node := range graphs {
+		P[node] = true
+	}
+
+	maxClique := make(map[string]bool)
+	bronKerbosch(R, P, X, &maxClique)
+
+	ans := make([]string, 0, len(maxClique))
+	for v := range maxClique {
+		ans = append(ans, v)
+	}
+	slices.Sort(ans)
+
+	return ans
+}
+
 func solve() {
+	start := time.Now()
 	for key := range graphs {
 		if string(key[0]) != CHIEF {
 			continue
@@ -67,7 +128,11 @@ func solve() {
 	}
 
 	puzzle_1 := len(cylicGraphs)
+	puzzle_2 := strings.Join(findInterconnected(), ",")
+
+	fmt.Println("time elapsed:", time.Since(start))
 	fmt.Println("puzzle 1:", puzzle_1)
+	fmt.Println("puzzle 2:", puzzle_2)
 }
 
 func main() {
