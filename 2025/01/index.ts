e63b5156a99f79f4817510d3b5e2a95b54b3ed5b
apwic <adiyansawicaksana@gmail.com>
2026-01-23 10:59:35 +0700

    2025 day 1 part 2

--- 2025/01/index.ts
+++ 2025/01/index.ts
@@ -2,33 +2,52 @@ import * as fs from "fs"
 
 const input = fs.readFileSync("input.txt", "utf-8").trim()
 const lines: string[] = input.split("\n")
-let start = 50;
-let ans = 0;
+let dial = 50;
+let ans1 = 0;
+let ans2 = 0;
 
 for (const line of lines) {
 	const rotation = line[0];
 	const rotationValueStr = line.slice(1);
 	let rotationValue = parseInt(rotationValueStr, 10);
+
+	// add div value for ans2
+	ans2 += Math.floor(rotationValue / 100)
+
 	// mod 100 for value that is more than 100
 	rotationValue %= 100
+	const dialBefore = dial;
 
 	if (rotation === "L") {
-		start = (start - rotationValue)
+		dial = (dial - rotationValue)
 	}
 
 	if (rotation === "R") {
-		start = (start + rotationValue)
+		dial = (dial + rotationValue)
 	}
 
-	if (start < 0) {
-		start += 100
-	} else if (start >= 100) {
-		start -= 100
+
+	if (dial < 0) {
+		if (dialBefore != 0) {
+			ans2 += 1
+		}
+
+		dial += 100
+	} else if (dial >= 100) {
+		if (dialBefore != 0) {
+			ans2 += 1
+		}
+
+		dial -= 100
+	} else if (dial == 0) {
+		ans2 += 1
 	}
 
-	if (start == 0) {
-		ans += 1
+	if (dial == 0) {
+		ans1 += 1
 	}
+
 }
 
-console.log(ans)
+console.log("Puzzle 1: ", ans1)
+console.log("Puzzle 2: ", ans2)
